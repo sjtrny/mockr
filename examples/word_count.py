62a33c1fae469af4085e5c554235955dd1fad15b
Stephen Tierney <sjtrny@gmail.com>
2018-05-11 12:38:39 +1000

    Fixed import in example

--- examples/word_count.py
+++ examples/word_count.py
@@ -7,7 +7,7 @@ Created on Wed Mar 14 08:30:48 2018
 """
 
 import re
-from mockmr import MockStreamingJob
+from mockmr.mockmr import MockStreamingJob
 
 WORD_RE = re.compile(r"[\w']+")
 
@@ -16,14 +16,18 @@ class WordCounter(MockStreamingJob):
     def map_fn(self, _, line):
         # yield each word in the line
         for word in WORD_RE.findall(line):
+            print("yield map")
             yield (word.lower(), 1)
         
     def reduce_fn(self, key, values):
+        print("yield reduce")
         yield (key, sum(values))
 
 input_file = open("MobyDick.txt", 'r')
 
-input_str = input_file.read()
+#input_str = input_file.read()
+
+input_str = "Hello World\nThis is World"
 
 job = WordCounter()
 
