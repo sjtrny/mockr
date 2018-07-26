5f63071278948e230ebac00ff268013f8584687a
Stephen Tierney <sjtrny@gmail.com>
2018-07-26 19:52:08 +1000

    Updated version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(path.dirname(__file__), 'README.md'), encoding='utf-8') as f
 
 setup(
     name = "mockmr",
-    version = "0.3",
+    version = "0.31",
     description = "A Python library for prototyping MapReduce jobs",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
